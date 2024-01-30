c4167c44e18a28ff0fe65e7dab77aefa5bdc6bc1
KHADGA-R <parajuli.kr@gmail.com>
2024-01-30 00:27:08 -0800

    adding slack command functionality

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ import slack
 import os
 from pathlib import Path 
 from dotenv import load_dotenv 
-from flask import Flask 
+from flask import Flask, request, Response
 from slackeventsapi import SlackEventAdapter
 
 
@@ -21,10 +21,10 @@ client = slack.WebClient(token=os.environ['SLACK_TOKEN'])
 
 BOT_ID = client.api_call("auth.test")['user_id']
 
-
+message_counts = {}
 
 #client.chat_postMessage(channel='#test', text="Hello World !")
-#client.chat_postMessage(channel='#test', text="Hello ! Thank you for reaching out to us. How can I help you today ?")
+client.chat_postMessage(channel='#test', text="Hello ! Thank you for reaching out to us. How can I help you today ?")
 
 @slack_event_adapter.on('message')
 def message(payLoad):
@@ -35,7 +35,25 @@ def message(payLoad):
 
 
     if BOT_ID != user_id:
+        if user_id in message_counts:
+            message_counts[user_id] += 1
+
+        else:
+            message_counts[user_id] = 1
         client.chat_postMessage(channel=channel_id, text="Thank you for reaching out to us. How can I help you today ?")
+
+@app.route('/message-count', methods=['POST'])
+def message_count():
+    data = request.form
+   #print(data)
+    user_id = data.get('user_id')
+    channel_id = data.get('channel_id')
+    message_count = message_counts.get(user_id, 0)
+
+    client.chat_postMessage(
+        channel=channel_id, text=f"Message: {message_count}")
+    return Response(), 200
+
 
 if __name__ == "__main__":
     app.run(debug=True)
